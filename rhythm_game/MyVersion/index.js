f15de70ddcb056da5c25b492976212818600fc20
JiaLong0209 <smt02091395@gmail.com>
2022-06-06 23:29:08 +0800

    v0.30 *track container

--- rhythm_game/MyVersion/index.js
+++ rhythm_game/MyVersion/index.js
@@ -1,5 +1,6 @@
 // 1. make a rough structure 2022/05/30 v0.10
-// 2. add keypress listener 2022/06/06 v0.20
+// 2. add keypress listener 2022/06/05 v0.20
+// 3. make track container 2022/06/06 v0.30
 let isHolding = {
     d: false,
     f: false,
@@ -25,24 +26,55 @@ let tracks;
 let combotText;
 let keypress;
 
+function initializeNotes (){
+    let noteElement;
+    let trackElement;
+
+    while(trackContainer.hasChildNodes()){  //當trackContainer有任何的子節點時，就清除該節點
+        trackContainer.removeChild(trackContainer.lastChild);
+    }
+
+    song.sheet.forEach((key,index)=>{
+        trackElement = document.createElement('div');
+        trackElement.classList.add('track');
+
+        key.notes.forEach((note)=>{
+            noteElement = document.createElement('div');
+            noteElement.classList.add('note');
+            console.log('add note');
+            noteElement.classList.add('note--' + index);
+            noteElement.style.backgroundColor = key.color;
+            noteElement.style.animationName = animation; //即為'moveDown'
+            noteElement.style.animationTimingFunction = 'linear';
+            noteElement.style.animationDuration = note.duration - speed + 's';
+            noteElement.style.animationDelay = note.delay + speed + 's';  
+            // noteElement.style.animationPlayState = 'paused';
+            trackElement.appendChild(noteElement);  //.track 增加.note子節點
+
+        });
+
+        trackContainer.appendChild(trackElement);  //在trackContainer增加.track子節點，沒有這行的話.track 跟 .note 會顯示不出來
+        tracks = document.querySelectorAll('.track');
+    })
+};
+
 function setupKeys (){
-    document.addEventListener('keydown',(event)=>{
+    document.addEventListener('keydown',(event)=>{ //當手壓下鍵盤的瞬間
         let keyIndex = getKeyIndex(event.key);
-        if(Object.keys(isHolding).indexOf(event.key) !== -1 && !isHolding[event.key]){
+        if(Object.keys(isHolding).indexOf(event.key) !== -1 && !isHolding[event.key]){  //當壓下的鍵在isHolding裡的index不為0 以及 該鍵isHolding不為true時
             isHolding[event.key] = true;
             keypress[keyIndex].style.display = 'block';
-            console.log(Object.keys(isHolding))
+
+            // console.log(Object.keys(isHolding))
         }
 
     })
 
-        
-    document.addEventListener("keyup",(event)=>{
+    document.addEventListener("keyup",(event)=>{ //當手放開鍵盤時
         if(Object.keys(isHolding).indexOf(event.key) !== -1){
             let keyIndex = getKeyIndex(event.key);
             isHolding[event.key] = false;
             keypress[keyIndex].style.display = 'none';
-            console.log(Object.keys(isHolding))
         }
 
 
@@ -62,10 +94,11 @@ function getKeyIndex (key){
     }
 }
 
+
 window.onload = function(){
     trackContainer = document.querySelector('.track-container');
     keypress = document.querySelectorAll(".keypress");
 
-
+    initializeNotes();
     setupKeys();
 }
---
+++ rhythm_game/MyVersion/song.js
@@ -0,0 +1,44 @@
+let d = {
+    color: "rgb(255,255,255)",
+    next: 0,
+    notes: [
+        {duration: 3, delay: 1},
+        {duration: 3, delay: 2},
+        {duration: 3, delay: 3}
+    ]
+};
+
+let f = {
+    color: "rgb(255,255,255)",
+    next: 0,
+    notes: [
+        {duration: 3, delay: 1},
+        {duration: 3, delay: 2},
+        {duration: 3, delay: 3}
+    ]
+};
+
+let j = {
+    color: "rgb(255,255,255)",
+    next: 0,
+    notes: [
+        {duration: 3, delay: 1},
+        {duration: 3, delay: 2},
+        {duration: 3, delay: 3}
+    ]
+};
+
+let k = {
+    color: "rgb(255,255,255)",
+    next: 0,
+    notes: [
+        {duration: 3, delay: 1},
+        {duration: 3, delay: 2},
+        {duration: 3, delay: 3}
+    ]
+};
+
+let song = {
+    duration: 30,
+    sheet: [d, f, j, k]
+};
