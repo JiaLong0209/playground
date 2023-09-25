8f230f8c8be99446ae0eb7afc8b9f2100f0ace87
Jialong0209 <smt02091395@gmail.com>
2023-09-26 02:30:36 +0800

    230926 Add blue and white particles

--- Artificial_life/script.js
+++ Artificial_life/script.js
@@ -34,7 +34,7 @@ function create(number, color){
 function rule(particle1, particle2, g){
     particle1.forEach((i) => {
         let a, b ,dx ,dy, d;
-        let fx = 0, fy = 0
+        let fx = 0, fy = 0, shrink = 0.998, acc = 0.1, speedLimit = 16;
         particle2.forEach((j) => {
             a = i;
             b = j;
@@ -42,14 +42,14 @@ function rule(particle1, particle2, g){
             dy = a.y - b.y;
             d = Math.sqrt(dx**2 + dy**2);
             if(d > 0){
-                if(d > 200) return;
-                let F = g * 1/d;
+                if(d > 250) return;
+                let F = g * 1/(d*1) * acc;
                 fx += (F * dx);
                 fy += (F * dy);
             }
         })
-        a.vx = (a.vx + fx) * 0.99;
-        a.vy = (a.vy + fy) * 0.99;
+        a.vx = (a.vx + fx) * shrink;
+        a.vy = (a.vy + fy) * shrink;
         a.x += a.vx;
         a.y += a.vy;
         if(a.x <= 0 || a.x > canvasWidth){
@@ -63,24 +63,43 @@ function rule(particle1, particle2, g){
             a.y = Math.abs(a.y) > Math.abs(a.y - canvasHeight) ? canvasHeight : 0;
             a.vy *= -1;
         }
-        
+        if(Math.sqrt(a.vx**2 + a.vy**2) > speedLimit) a.vx *= 0, a.vy *= 0
         // if(a.x <= 0 || a.x > canvasWidth) a.vx *= -1;
         // if(a.y <= 0 || a.y > canvasHeight) a.vy *= -1;
     })
 
 }
 
-yellow = create(50, 'yellow');
-red = create(30, 'red');
-
+yellow = create(200, 'yellow');
+red = create(170, 'red');
+blue = create(200, 'blue');
+white = create(1, 'white');
 function update(){
     m.clearRect(0, 0, canvasWidth, canvasHeight);
-    rule(yellow, yellow, -0.1);
-    rule(red, yellow, 0.1)
-    rule(yellow, red, 0.5)
+    rule(yellow, yellow, -0.008);
+    rule(yellow, red, -0.032);
+    rule(yellow, blue, 0.04);
+    rule(yellow, white, 1.4);
+    rule(red, red, -0.01);
+    rule(red, yellow, -0.04);
+    rule(red, blue, -0.04);
+    rule(red, white, 1.4);
+    rule(blue, blue, -0.01);
+    rule(blue, red, -0.024);
+    rule(blue, yellow, 0.04);
+    rule(blue, white, 1.4);
+    rule(white, white, 0.001);
+    rule(white, yellow, -0.005);
+    rule(white, red, -0.005);
+    rule(white, blue, -0.005);
+
     draw(0, 0, '#000', Math.max(canvasWidth, canvasHeight));
     particles.forEach((i)=>{
-        draw(i.x, i.y, i.color, 5);
+        if(i.color == 'white'){
+            draw(i.x, i.y, i.color, 10);
+        }else{
+            draw(i.x, i.y, i.color, 4);
+        }
     })
     requestAnimationFrame(update);
 }
